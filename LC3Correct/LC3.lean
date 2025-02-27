e3b19739a93809eb8bec4629cbd929755e09218d
Derek Sorensen <d@dhsorens.com>
2025-02-26 21:30:03 -0500

    inital commit for FV work on the LC3

---
+++ LC3Correct/LC3.lean
@@ -0,0 +1,11 @@
+import LC3Lean.Memory
+import LC3Lean.Registers
+import LC3Lean.Instructions
+import LC3Lean.Execution
+import LC3Lean.Trap
+import Main
+
+-- TODO
+-- IO functionality
+-- Execution
+-- Trap
---
+++ LC3Correct/README.md
@@ -0,0 +1,3 @@
+# Specification and Proofs of Correctness 
+
+This directory has a specification and formal proofs of correctness of my implementation of the LC3 VM. 
